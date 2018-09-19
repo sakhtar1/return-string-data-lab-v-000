62deff9e0e4c9440912590310892b706637d6635
Sharmina Akhtar <sakht318@gmail.com>
2018-09-19 22:08:25 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,5 @@
 class ProductsController < ApplicationController
 
-  before_action :set_product, only: [:show, :edit, :update]
-
     def index
       @products = Product.all
     end
@@ -32,10 +30,6 @@ class ProductsController < ApplicationController
 
 
 private
-# Use callbacks to share common setup or constraints between actions.
-  def set_product
-    @product = Product.find(params[:id])
-  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def product_params
