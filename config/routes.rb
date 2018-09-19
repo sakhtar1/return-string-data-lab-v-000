4fbe9ae8d575e19cf0c6fd9556be48b3a024baf5
Sharmina Akhtar <sakht318@gmail.com>
2018-09-19 19:57:47 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :invoices
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
   get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
 end
