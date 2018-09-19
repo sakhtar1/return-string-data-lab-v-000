9e34482d287ce75cf7d9aa5cf7264015752d2ccc
Sharmina Akhtar <sakht318@gmail.com>
2018-09-19 19:19:28 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,4 +14,40 @@ class ProductsController < ApplicationController
    render plain: product.description
  end
 
+ def show
+  set_post
 end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.create(post_params)
+  @post.save
+  redirect_to post_path(@post)
+end
+
+def edit
+end
+
+def update
+  @post.update(post_params)
+  redirect_to post_path(@post)
+end
+
+
+
+private
+# Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:title, :description)
+  end
+end
+
+
