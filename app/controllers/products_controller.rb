4b528014efdeb6b19aa5d8bbd56f1ececf0e5c42
Sharmina Akhtar <sakht318@gmail.com>
2018-09-19 19:17:10 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < ApplicationController
 
   def index
-
     @products = Product.all
-
   end
+
+  def inventory
+   product = Product.find(params[:id])
+   render plain: product.inventory > 0 ? true : false
+ end
 
 end
