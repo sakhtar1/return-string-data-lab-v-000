a5b78d53075a325e4c9717cfdac5bec41b0b1583
Sharmina Akhtar <sakht318@gmail.com>
2018-09-19 22:09:23 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
 
     def inventory
      product = Product.find(params[:id])
-     if product.inventory === 0
+     if product.inventory == 0
        render plain: false
      else
        render plain: true
